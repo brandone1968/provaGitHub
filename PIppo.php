6d5f426808d0d476c2aa6e84fe37753ee500438a
Massimo Brandone <massimo.brandone@gmail.com>
2024-07-02 19:43:31 +0200

    altra prova

--- PIppo.php
+++ PIppo.php
@@ -10,12 +10,25 @@ class Pippo
         echo "addio";
     }
 
+    function ScrivoUffa() {
+        echo "uffa";
+    }
+
+    function ScrivoUffa2() {
+        echo "uffa2";
+    }
+    
+
     function ScrivoBuongiorno() {
         echo "buongiorno";
     }
 
     function ScrivoBuonasera() {
         echo "buonasera";
+    }
+
+    function ScrivoAltraProva() {
+        echo "altra prova";
     }
     
 }
